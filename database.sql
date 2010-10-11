8265371519c9f16dc2b455cd9abc54c34d743526
Rick Tonoli <rick@tonoli.co.za>
2010-10-11 12:57:02 +0200

    Fixed database creation skill

--- database.sql
+++ database.sql
@@ -4,13 +4,14 @@ USE 'lctn';
 --
 
 DROP TABLE IF EXISTS `url`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `url` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `longurl` varchar(255) NOT NULL,
-  `shorturl` varchar(30)) NOT NULL,
-  `timestamp` int(11) NOT NULL,
+  `longurl` varchar(100) NOT NULL,
+  `shorturl` varchar(30) NOT NULL,
+  `created` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
