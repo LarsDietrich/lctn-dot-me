5e6296e8ac1ae1ad21cbbf61fa224186059ecebe
Rick Tonoli <rick@tonoli.co.za>
2010-10-11 12:12:08 +0200

    Added initial files

---
+++ database.sql
@@ -0,0 +1,16 @@
+USE 'lctn';
+--
+-- Table structure for table `url`
+--
+
+DROP TABLE IF EXISTS `url`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `url` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `longurl` varchar(255) NOT NULL,
+  `shorturl` varchar(30)) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
