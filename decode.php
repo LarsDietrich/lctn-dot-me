29f0b0c93da2a4c66edf63ef63ca67f05525d611
Rick Tonoli <rick@tonoli.co.za>
2010-10-15 15:21:57 +0200

    Added redirect to 404 page when no page found

--- decode.php
+++ decode.php
@@ -5,6 +5,11 @@ require("includes/functions.php");
 $url= mysql_escape_string($_GET["decode"]);
 $longUrl = getLongUrl($url);
 
+if ($longUrl == "") {
+	$longUrl = "http://test.lctn.me/404.php";
+} 
+
 header("location:$longUrl");
+
 
 ?>
