4a91d922cb691b2ff858d24b6f0b19c07bc5e2a8
Rick Tonoli <rick@tonoli.co.za>
2010-11-17 15:13:06 +0200

    Cleaned up code

--- contact.php
+++ contact.php
@@ -4,59 +4,55 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 		<title>jQuery Ajax Forms | Trevor Davis</title>
 		<link href="css/contact.css" type="text/css" rel="stylesheet" media="screen,projection" />
-		
 		<script type="text/javascript">
-		
-		$(document).ready(function(){
-			$("#submit").click(function(){					   				   
-				$("#error").hide();
-				var hasError = false;
-				var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-				var errorColor = "#FF0000";
-				var emailFromVal = $("#emailFrom").val();
-				$('#emailFrom').css("background-color", "white");
-				if(emailFromVal == '') {
-					$('#emailFrom').css("background-color", errorColor);
-					hasError = true;
-				} else if(!emailReg.test(emailFromVal)) {	
-					$('#emailFrom').css("background-color", errorColor);
-					hasError = true;
-				}
-				
-				var messageVal = $("#message").val();
-				$('#message').css("background-color", "white");
-				if(messageVal == '') {
-					$('#message').css("background-color", errorColor);
-					hasError = true;
-				}
-				
-				
-				if(hasError == false) {
-					$(this).hide();
-					$("#sendEmail li.buttons").append('<img src="/images/loading.gif" alt="Loading" id="loading" />');
+			$(document).ready(function(){
+				$("#submit").click(function(){					   				   
+					$("#error").hide();
+					var emailTo = "rick@tonoli.co.za";
+					var subject = "An email from lctn.me's contact form";
+					var hasError = false;
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					var errorColor = "#FF0000";
+					var emailFromVal = $("#emailFrom").val();
+					$('#emailFrom').css("background-color", "white");
+					if(emailFromVal == '') {
+						$('#emailFrom').css("background-color", errorColor);
+						hasError = true;
+					} else if(!emailReg.test(emailFromVal)) {	
+						$('#emailFrom').css("background-color", errorColor);
+						hasError = true;
+					}
 					
-					$.post("email.php",
-		   				{ emailTo: "rick@tonoli.co.za", emailFrom: emailFromVal, subject: "Contact from lctn.me", message: messageVal },
-		   					function(data){
-								$("#sendEmail").slideUp("normal", function() {				   
-									$("#sendEmail").before('<h1>Success</h1><p>Your email was sent.</p>');											
-								});
-		   					}
-						 );
-				}
-				
-				return false;
-			});						   
-		});
-		
+					var messageVal = $("#message").val();
+					$('#message').css("background-color", "white");
+					if(messageVal == '') {
+						$('#message').css("background-color", errorColor);
+						hasError = true;
+					}
+					
+					
+					if(hasError == false) {
+						$(this).hide();
+						$("#sendEmail li.buttons").append('<img src="/images/loading.gif" alt="Loading" id="loading" />');
+						
+						$.post("email.php",
+			   				{ emailTo: emailTo, emailFrom: emailFromVal, subject: subject, message: messageVal },
+			   					function(data){
+									$("#sendEmail").slideUp("normal", function() {				   
+										$("#sendEmail").before('<h1>Success</h1><p>Your email was sent. Press ESC or click the close button to continue.</p>');											
+									});
+			   					}
+							 );
+					}
+					
+					return false;
+				});						   
+			});
 		</script>
-	
 	</head>
-	
 	<body>
-	
 		<div id="container">
-		<?php include('verify.php'); ?>
+			<?php include('verify.php'); ?>
 			<form action="/" method="post" id="sendEmail">
 				<p>If you find something that doesn't work, or think you have a cool idea, please let me know!</p>
 				<ol class="forms">
