4032b94c78c1d0924386b44bdcf7366dbc6b2f3f
Rick Tonoli <rick@tonoli.co.za>
2010-12-06 16:45:32 +0200

    FIX: Problem with checkboxes showing incorrect state on null value

--- src/config.php
+++ src/config.php
@@ -9,7 +9,7 @@
 	    <script type="text/javascript">
 	    	$("input[class=config]").each(function () {
 				var value = $.cookie("option_" + $(this).attr("id"));
-				if (value == "true" || value == null) {
+				if (value == "true") {
 					$(this).attr("checked",true);
 				} else {
 					$(this).attr("checked",false);
