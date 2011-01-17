1eec2d68d9602a2f765fe983fa2adf89659255b3
Rick Tonoli <rick@tonoli.co.za>
2011-01-17 13:40:51 +0200

    Fixed redirect error

--- src/decode.php
+++ src/decode.php
@@ -1,18 +1,19 @@
-<!-- Decode the incoming short link and generate a long url -->
 <?php
-
 require("includes/functions.php");
-//$_url="rse1";
-$_url= mysql_escape_string($_GET["decode"]);
-$_longUrl = getLongUrl($_url);
-
-if ($_longUrl == "") {
-	$result = "error/404.php";
+$input = $_GET["decode"];
+$output = "";
+//$input = "rse1";
+$url= mysql_escape_string($input);
+$longUrl = getLongUrl($url);
+if ($longUrl == "") {
+	$output = "error/404.php";
 	die();
 } else {
-	$result = "?q=" . base64_encode($_longUrl);
+	$output = "index.php?q=" . base64_encode($longUrl);
 }
 
-header("location:$result");
+redirectTo($output)
+
+//header("Location: $output", TRUE, 301);
 
 ?>
