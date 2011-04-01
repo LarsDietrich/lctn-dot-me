467e320113390fbcf37cbdbb4dd83ecbb5a63a27
Rick Tonoli <rick@tonoli.co.za>
2011-04-01 13:06:16 +0200

    Added getclicky tracking code

--- src/index.php
+++ src/index.php
@@ -25,6 +25,10 @@
 
 	<body onload="load()">
 
+		<script src="//static.getclicky.com/js" type="text/javascript"></script>
+		<script type="text/javascript">try{ clicky.init(66405769); }catch(e){}</script>
+		<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66405769ns.gif" /></p></noscript>
+
 		<div id="map_canvas" class="map_background"></div>
 
 		<div style="z-index: 10000; display: inline;" id="message_container" class="message-box">
