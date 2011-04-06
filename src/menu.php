58e80b5d8bd2eafec83f3e8102854bf372b96d05
Rick Tonoli <rick@tonoli.co.za>
2011-04-07 00:04:10 +0200

    Removed test data, changed name of General page to Local

--- src/menu.php
+++ src/menu.php
@@ -7,17 +7,18 @@
 
 	    <li><img src="/images/topnav_bg_divider.png"/></li>
 
-		<li><a><label onclick="setConfigOption(this);" id="general" title="Open to see some general information about the location. Currently shows weather, coordinates and time related information." class="config" >General</label></a></li>
- 		<li><a><label title="Toggle the window showing places around the current location" id="places" class="config" onclick="setConfigOption(this);">Places</label></a></li>
-		<li><a><label title="Open to get directions between two points, updates the map to show the route and gives you a friendly route description." id="route" class="config" onclick="setConfigOption(this);">Directions</label></a></li>
-		<li><a><label title="Open this window to show you a street view of the chosen location. Not available everywhere." id="streetview" class="config" onclick="setConfigOption(this);">Streetview</label></a></li>
-		<li><a><label title="Open to see wikipedia articles in the area with short description, click to link through to actual article. Can filter by range." id="wiki" class="config" onclick="setConfigOption(this);">Wikipedia</label></a></li>
-		<li><a><label title="Based on your location, will show you tweets posted for people that have enabled location based tweets. Filter on range and text." id="twitter" class="config" onclick="setConfigOption(this);">Twitter</label></a></li>
-		<li><a><label title="Show photos in the area based on the Flickr service." id="picture" class="config" onclick="setConfigOption(this);">Photos</label></a></li>
-		<li><a><label title="Open to see available webcams in the area, mostly still image but some time capture video is available. Filter by range." id="webcam" class="config" onclick="setConfigOption(this);">Webcams</label></a></li>
+		<li><a><label onclick="setConfigOption(this);" id="general" title="Open to see some localized information about the location. Shows weather, coordinates and time related information." class="config" >Local</label></a></li>
+ 		<li><a><label title="Show places of interest around the current location. Data supplied by Foursquare." id="places" class="config" onclick="setConfigOption(this);">Places</label></a></li>
+		<li><a><label title="Get directions between two points, updates the map to show the route and gives you a friendly route description." id="route" class="config" onclick="setConfigOption(this);">Directions</label></a></li>
+		<li><a><label title="Show you a street view of the chosen location. Not available everywhere." id="streetview" class="config" onclick="setConfigOption(this);">Streetview</label></a></li>
+		<li><a><label title="Wikipedia articles in the area with short description, click to link through to actual article. Can filter by range." id="wiki" class="config" onclick="setConfigOption(this);">Wikipedia</label></a></li>
+		<li><a><label title="Show tweets by people that have enabled location based tweets. Can filter on range and text." id="twitter" class="config" onclick="setConfigOption(this);">Twitter</label></a></li>
+		<li><a><label title="Show publically available photos in the area (Uses Flickr and Instagram services). Can filter by range." id="picture" class="config" onclick="setConfigOption(this);">Photos</label></a></li>
+		<li><a><label title="See still image and time capture webcams in the area. Can filter by range." id="webcam" class="config" onclick="setConfigOption(this);">Webcams</label></a></li>
 	    <li><img src="/images/topnav_bg_divider.png"/></li>
 		<?php if (isset($_COOKIE["fbs_179640572057712"])) {?>
-			<li><a><label title="Personalized information about what you've done" id="user" class="config" onclick="setConfigOption(this);">You</label></a></li>
+<!-- 			<li><a><label title="Personalized information about what you've done" id="user" class="config" onclick="setConfigOption(this);">You</label></a></li>
+-->
 		<?php }?>
 
 	</ul>
